5f4e2d62d5b4bd5722150d6960e16fe91aa7e948
hammad sunsara <hammadsunsara9@gmail.com>
2025-09-25 18:30:32 +0530

    error solved

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const profileRoutes = require("./routes/profileRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const serviceRoutes = require("./routes/serviceRoutes");
 const userTags = require("./routes/userTags");
-const location = require("./routes/location");
+const locationRoutes = require("./routes/location");
 const app = express();
 
 // Middleware
@@ -34,7 +34,7 @@ app.use("/api", profileRoutes);
 app.use("/api/admin/categories", categoryRoutes);
 app.use("/api/service", serviceRoutes);
 app.use("/api/onbording", userTags);
- app.use("api/user", location);
+ app.use("/api/user", locationRoutes);
 
 // Connect to MongoDB (live Atlas)
 mongoose

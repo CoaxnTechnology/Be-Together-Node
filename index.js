c97ddd24f2a452dbdeb68d400e75eee0bd88b413
hammad sunsara <hammadsunsara9@gmail.com>
2025-09-29 18:23:06 +0530

    update

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use("/api/admin/categories", categoryRoutes);
 app.use("/api/service", serviceRoutes);
 app.use("/api/onbording", userTags);
  app.use("/api/user", locationRoutes);
-app.use("api",ReviewRoutes)
+app.use("/api",ReviewRoutes)
 // Connect to MongoDB (live Atlas)
 mongoose
   .connect(process.env.MONGO_URI, {

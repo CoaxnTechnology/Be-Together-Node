b522c6f58931c5433b45d00590d0a8c7f04394b7
hammad sunsara <hammadsunsara9@gmail.com>
2025-09-29 18:23:53 +0530

    auth added

--- routes/ReviewRoutes.js
+++ routes/ReviewRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createReview, getServiceReviews } = require("../controller/Review");
+const authMiddleware = require("../Middleware/authMiddleware");
 
 const router = express.Router();
-router.post("/createreviews", createReview);
+router.post("/createreviews",authMiddleware, createReview);
 router.get("/getreviews", getServiceReviews);
 
 module.exports = router;

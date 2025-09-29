d630bcf654180d79a0ef7eff9d27605a7998634b
hammad sunsara <hammadsunsara9@gmail.com>
2025-09-29 12:56:58 +0530

    create service changes for city

--- model/Service.js
+++ model/Service.js
@@ -7,7 +7,11 @@ const serviceSchema = new mongoose.Schema({
   price: { type: Number, default: 0 },
   description: { type: String, default: null },
 
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
   tags: { type: [String], default: [] },
 
   max_participants: { type: Number, default: 1 },
@@ -16,12 +20,16 @@ const serviceSchema = new mongoose.Schema({
   location_name: { type: String, default: null },
   latitude: { type: Number, default: null },
   longitude: { type: Number, default: null },
-
+  city: { type: String, default: null},
   // Who created this service
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
   // Type of service
-  service_type: { type: String, enum: ["one_time", "recurring"], default: "one_time" },
+  service_type: {
+    type: String,
+    enum: ["one_time", "recurring"],
+    default: "one_time",
+  },
 
   // one_time service
   date: { type: Date, default: null },
@@ -32,17 +40,17 @@ const serviceSchema = new mongoose.Schema({
   recurring_schedule: {
     type: [
       {
-        day: { type: String, required: true },        // e.g. "Monday"
+        day: { type: String, required: true }, // e.g. "Monday"
         start_time: { type: String, required: true }, // "09:00"
-        end_time: { type: String, required: true },   // "11:00"
-        date: { type: Date, required: true }          // computed first date >= start_date
-      }
+        end_time: { type: String, required: true }, // "11:00"
+        date: { type: Date, required: true }, // computed first date >= start_date
+      },
     ],
-    default: []
+    default: [],
   },
 
   created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  updated_at: { type: Date, default: Date.now },
 });
 
 serviceSchema.pre("save", function (next) {

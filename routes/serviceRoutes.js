a939c951a445dabaa6e4bfb57fa02b0dc02494e1
hammad sunsara <hammadsunsara9@gmail.com>
2025-10-01 11:04:34 +0530

    new changes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const serviceController = require("../controller/serviceController");
 const auth = require("../Middleware/authMiddleware");
-router.post("/create", auth, serviceController.createService);
+//router.post("/create", auth, serviceController.createService);
 router.post("/get", serviceController.getServices);
 router.post("/user/search", serviceController.getInterestedUsers);
 router.get("/getall", serviceController.getAllServices);

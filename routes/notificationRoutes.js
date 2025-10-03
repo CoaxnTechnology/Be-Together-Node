68309fe089ff118a03554262f7276ce78ff6ff11
hammad sunsara <hammadsunsara9@gmail.com>
2025-10-03 18:48:07 +0530

    changes

--- routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendTestNotification } = require("../controller/notificationController");
-
-// POST /api/notifications/test
-router.post("/test", sendTestNotification);
-
-module.exports = router;

b5386aa461f05eb218256395a141635d2f9d0dfa
hammad sunsara <hammadsunsara9@gmail.com>
2025-10-07 11:54:00 +0530

    changes

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -6,7 +6,7 @@ const { createCategory, getAllCategories } = require("../controller/Admin");
 const upload = multer({ dest: "uploads/" });
 
 // POST /api/admin/category/create
-router.post("/create", upload.single("image"), createCategory);
+//router.post("/create", upload.single("image"), createCategory);
 router.get("/getall", getAllCategories);
 
 module.exports = router;

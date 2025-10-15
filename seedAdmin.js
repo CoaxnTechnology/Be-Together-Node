a1f8b86049e4053d92fd12381c8ad4b02f1ca3cd
hammad sunsara <hammadsunsara9@gmail.com>
2025-10-15 18:12:44 +0530

    changes

---
+++ seedAdmin.js
@@ -0,0 +1,33 @@
+// seedAdmin.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Admin = require("./model/Admin");
+require("dotenv").config();
+const DB_URI = process.env.MONGO_URI; // replace with your DB URI
+
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(async () => {
+    console.log("Connected to MongoDB");
+
+    const existingAdmin = await Admin.findOne({ email: "admin01" });
+    if (existingAdmin) {
+      console.log("Admin already exists");
+      process.exit(0);
+    }
+
+    const hashedPassword = await bcrypt.hash("admin@1212", 10);
+
+    const admin = new Admin({
+      name: "Admin User",
+      email: "admin01@gmail.com",
+      hashed_password: hashedPassword,
+      is_active: true,
+    });
+    await admin.save();
+    console.log("Admin created successfully");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  });

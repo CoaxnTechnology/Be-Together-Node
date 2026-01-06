1f5053be67806dda6f6631d75b89772668070970
hammad sunsara <hammadsunsara9@gmail.com>
2026-01-06 16:39:29 +0530

    new changes

--- index.js
+++ index.js
@@ -58,12 +58,10 @@ app.post(
     }
   }
 );
-app.post("/webhook/github", (req, res) => {
-  console.log("🔥 FRONTEND WEBHOOK HIT");
-
-  exec("bash /var/www/testing/admin/deploy.sh > /dev/null 2>&1 &");
-
-  res.status(200).json({ message: "received" });
+app.post("/webhook/testing", (req, res) => {
+  console.log("🔥 WEBHOOK TEST HIT");
+  exec("bash /var/www/testing/admin/deploy.sh");
+  res.json({ message: "frontend testing deploy started" });
 });
 
 // Middleware

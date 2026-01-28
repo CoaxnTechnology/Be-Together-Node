615a3190d6b7b4123af98e29979004c7f66c1e90
hammad sunsara <hammadsunsara9@gmail.com>
2026-01-28 12:17:01 +0530

    new code add

--- routes/promotionSubscription.Routes.js
+++ routes/promotionSubscription.Routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createPromotionSubscription } = require("../controller/promotionSubscription.controller");
 const router = express.Router();
 router.post(
-  "/promotion/create-subscription",
+  "/create-subscription",
   createPromotionSubscription
 );
 

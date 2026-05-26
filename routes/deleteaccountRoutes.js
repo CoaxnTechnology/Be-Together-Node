66cf0eae42a0d1f344e52909ff0f6782288af99c
hammad sunsara <hammadsunsara9@gmail.com>
2026-05-26 11:55:44 +0530

    new changes

--- routes/deleteaccountRoutes.js
+++ routes/deleteaccountRoutes.js
@@ -8,11 +8,12 @@ const {
   deleteAccount,
 } = require("../controller/deleteaccountcontroller");
 const auth = require("../Middleware/authMiddleware");
+const adminAuth = require("../Middleware/adminAuth");
 // deleted users list
 router.get("/deleted-users", getDeletedUsers);
 
 // deleted user details
-router.get("/deleted-users/:backupId", getDeletedUserById);
+router.get("/deleted-users/:backupId",adminAuth, getDeletedUserById);
 
 // DELETE ACCOUNT
 router.delete("/delete-account", auth, deleteAccount);

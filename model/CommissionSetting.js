479dccfc939017951df954009c62e4743a4b0ebf
hammad sunsara <hammadsunsara9@gmail.com>
2026-06-23 12:48:12 +0530

    new logic add

--- model/CommissionSetting.js
+++ model/CommissionSetting.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 
 const commissionSchema = new mongoose.Schema({
-  percentage: { type: Number, default: 20 }, // admin default
-  updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  updatedAt: { type: Date, default: Date.now },
-});
+  providerCommissionPercentage: {
+    type: Number,
+    default: 8,
+  },
 
+  customerCommissionPercentage: {
+    type: Number,
+    default: 4,
+  },
+
+  updatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 module.exports = mongoose.model("CommissionSetting", commissionSchema);

23f4fb2ff2fb4b04ea82e2108f8737bd3d684bab
CoaxnTechnology <123242886+CoaxnTechnology@users.noreply.github.com>
2026-07-21 14:09:41 +0530

    Add Trust & Safety Policy endpoint

--- index.js
+++ index.js
@@ -158,6 +158,12 @@ app.get("/api/community-guidelines", (req, res) => {
     path.join(__dirname, "templates", "community-guidelines.html")
   );
 });
+app.get("/api/trust-safety", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "templates", "Trust & Safety Policy.html")
+  );
+});
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, "public")));
 

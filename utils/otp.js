6fa1acc9039ef1dbf147e5cd8bb46b0a9dfba344
CoaxnTechnology <123242886+CoaxnTechnology@users.noreply.github.com>
2026-08-03 11:03:05 +0530

    Extend OTP expiry time to 5 minutes

--- utils/otp.js
+++ utils/otp.js
@@ -2,7 +2,7 @@ const random = require("crypto");
 
 function generateOTP() {
   const otp = ("" + Math.floor(1000 + Math.random() * 9000)).substring(0, 4); // 4-digit
-  const expiry = new Date(Date.now() + 2 * 60 * 1000); // 2 min expiry
+  const expiry = new Date(Date.now() + 5 * 60 * 1000); // 5 min expiry
   return { otp, expiry };
 }
 

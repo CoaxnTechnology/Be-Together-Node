edcbf69180868705a686de5067efa7bd25b22a99
CoaxnTechnology <123242886+CoaxnTechnology@users.noreply.github.com>
2026-08-05 16:16:10 +0530

    Comment out ApiLog schema and model export
    Commented out the entire ApiLog schema definition and export.

--- model/ApiLog.js
+++ model/ApiLog.js
@@ -1,68 +1,68 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const apiLogSchema = new mongoose.Schema(
-  {
-    method: {
-      type: String,
-      required: true,
-    },
+// const apiLogSchema = new mongoose.Schema(
+//   {
+//     method: {
+//       type: String,
+//       required: true,
+//     },
 
-    endpoint: {
-      type: String,
-      required: true,
-    },
+//     endpoint: {
+//       type: String,
+//       required: true,
+//     },
 
-    statusCode: {
-      type: Number,
-      required: true,
-    },
+//     statusCode: {
+//       type: Number,
+//       required: true,
+//     },
 
-    success: {
-      type: Boolean,
-      required: true,
-    },
+//     success: {
+//       type: Boolean,
+//       required: true,
+//     },
 
-    message: {
-      type: String,
-      default: null,
-    },
+//     message: {
+//       type: String,
+//       default: null,
+//     },
 
-    error: {
-      type: String,
-      default: null,
-    },
+//     error: {
+//       type: String,
+//       default: null,
+//     },
 
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      default: null,
-    },
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       default: null,
+//     },
 
-    adminId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Admin",
-      default: null,
-    },
+//     adminId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Admin",
+//       default: null,
+//     },
 
-    ip: {
-      type: String,
-      default: null,
-    },
+//     ip: {
+//       type: String,
+//       default: null,
+//     },
 
-    duration: {
-      type: Number,
-      default: 0, // milliseconds
-    },
+//     duration: {
+//       type: Number,
+//       default: 0, // milliseconds
+//     },
 
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      expires: 60 * 60 * 24 * 30, // Auto delete after 30 days
-    },
-  },
-  {
-    versionKey: false,
-  },
-);
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//       expires: 60 * 60 * 24 * 30, // Auto delete after 30 days
+//     },
+//   },
+//   {
+//     versionKey: false,
+//   },
+// );
 
-module.exports = mongoose.model("ApiLog", apiLogSchema);
+// module.exports = mongoose.model("ApiLog", apiLogSchema);
